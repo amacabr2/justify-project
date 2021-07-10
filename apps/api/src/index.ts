5a58bbf5c0f9774c97a156d5d6a24fd50f0c30ba
amacabr2 <anthony.macabrey70@gmail.com>
2021-07-10 12:55:14 +0200

    init api and justify route

---
+++ apps/api/src/index.ts
@@ -0,0 +1,23 @@
+import express, {Request, Response} from 'express';
+import bodyParser from 'body-parser';
+import {justify} from '@justify-project/justify';
+
+const PORT = 3000;
+const app = express();
+
+app.use(bodyParser.json());
+
+app.post('/api/justify', (req: Request, res: Response) => {
+    const text = req.body.text;
+    if (text?.length) {
+        res.status(201).json({
+            justify: justify(text.toString())
+        });
+    }
+    res.json({
+        error: 'ZUT',
+    });
+})
+
+app.listen(PORT, () => console.log(`Server start on port ${PORT}!`))
+
